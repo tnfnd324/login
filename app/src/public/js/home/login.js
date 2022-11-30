81235b454fc081a6d0faeddb0a24dd8661ec4f9a
tnfnd324 <tnfnd324@naver.com>
2022-11-30 14:23:39 +0900

    fetch : requires login session

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -9,13 +9,19 @@ loginBtn = document.querySelector("button");
 loginBtn.addEventListener("click", login);
 
 function login() { 
-    console.log(id.value);
-    console.log(psword.value);
-
     const req = {      //request 오브젝트
         id: id.value,
         psword: psword.value,
     };
 
     console.log(req);
+    console.log(JSON.stringify(req));
+
+    fetch("/login", {
+        method: "POST",
+        headers: {
+            "Content-Type" : "application/json",
+        },
+        body: JSON>stringify(req),
+    });
 }
