599fa3ee8754112b0cfc84bd611d928fa47291e3
tnfnd324 <tnfnd324@naver.com>
2022-12-05 13:29:43 +0900

    19th Rollback Point

---
+++ login-lecture2/app/src/routes/home/home.ctrl.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const User = require("../../models/User");
+
+const output = {
+  home: (req, res) => {
+    res.render("home/index");
+  },
+  login: (req, res) => {
+    res.render("home/login");
+  },
+};
+
+const process = {
+  login: (req, res) => {
+    const user = new User(req.body);
+    const response = user.login();
+    return res.json(response);
+  },
+};
+
+module.exports = {
+  output,
+  process,
+};
