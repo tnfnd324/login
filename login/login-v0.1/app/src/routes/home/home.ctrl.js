078bf79fa20c9446f0f54487e569f499d74ea0f8
System Administrator <root@172.20.nate.com>
2022-12-16 14:29:20 +0900

    12.16

--- login/login-v0.1/app/src/routes/home/home.ctrl.js
+++ login/login-v0.1/app/src/routes/home/home.ctrl.js
@@ -1,15 +1,21 @@
 "use strict";
 
+const logger = require("../../config/logger");
 const User = require("../../models/User");
 
 const output = {
   home: (req, res) => {
+    logger.info(`get / 200 "Move to Home"`);
     res.render("home/index");
   },
   login: (req, res) => {
+    logger.info(`get /login 200 "Move to login"`);
+
     res.render("home/login");
   },
   register: (req, res) => {
+    logger.info(`get /register 200 "Move to register"`);
+
     res.render("home/register");
   },
 };
@@ -18,11 +24,17 @@ const process = {
   login: async (req, res) => {
     const user = new User(req.body);
     const response = await user.login();
+    logger.info(
+      `POST /login 200 Response: "success: ${response.success}, msg: ${response.msg}"`
+    );
     return res.json(response);
   },
   register: async (req, res) => {
     const user = new User(req.body);
-    const response = await user.register();   //register의 반환값을 받아서
+    const response = await user.register();
+    logger.info(
+      `POST /register 200 Response: "success: ${response.success}, msg: ${response.msg}"`
+    );
     return res.json(response);          //json 메서드를 통해서 클라이언트에게 응답
   },
 };
